413d69bc55699f7318d54f5a4e8306d09ce59a49
Controlador de Inventarios <verificadorsps@cafemaya.co>
2026-08-19 10:05:31 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='bia-control-v2.8.1';
+const CACHE_NAME='bia-control-v2.8.2';
 const APP_SHELL=[
   './','./index.html','./cronometros.html','./cronometros.js','./supabase-config.js',
   './pwa.js','./offline.html',
